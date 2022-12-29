d1b56c54269f035777590bcbc9d5a49cf2ad3e01
lucas <yangzhoulong@icloud.com>
2022-12-29 17:59:41 +0800

    fix: 修复样式层级问题

--- src/view/popup/index.tsx
+++ src/view/popup/index.tsx
@@ -55,7 +55,7 @@ const Popup: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    chrome.runtime.onMessage.addListener(function (
+    window?.chrome?.runtime?.onMessage?.addListener(function (
       request,
       sender,
       sendResponse
